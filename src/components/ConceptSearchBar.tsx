ad43948b3c764f0111f53f736ecdd0fc547b67ef
Shrey Joshi <shreyjoshi2004@gmail.com>
2023-06-01 18:55:35 -0500

    fix searchbar large searches by making API calls

--- src/components/ConceptSearchBar.tsx
+++ src/components/ConceptSearchBar.tsx
@@ -7,13 +7,43 @@ import {
     AutoCompleteList,
 } from "@choc-ui/chakra-autocomplete";
 import { Item } from "@choc-ui/chakra-autocomplete";
-
-import concepts from "../concepts";
+import { loadingState } from "../atoms";
+import { useRecoilState } from "recoil";
 
 export const ConceptSearchBar = () => {
     const addConceptToEq = (params: { item: Item, selectMethod: "mouse" | "keyboard" | null, isNewInput?: boolean }) => {
         console.log(params.item.value)
     }
+
+    const [searchLoading, setSearchLoading] = useRecoilState(loadingState)
+
+    const queryBackend = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let query = event.target.value;
+        if (query.length > 0) {
+            console.log(query)
+            setSearchLoading(true)
+            fetch(`https://shreyj1729--autosuggest.modal.run/?query=${query}&limit=100`)
+                .then(response => response.json())
+                .then(data => {
+                    setConcepts(data)
+                    setSearchLoading(false)
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }
+    }
+
+    // React.useEffect(() => {
+    //     console.log("setting loading to false")
+    //     if (numReqs === 0) {
+    //         // console.log("setting loading to false")
+    //         setSearchLoading(false)
+    //     }
+    // }, [numReqs, setSearchLoading])
+
+
+    const [concepts, setConcepts] = React.useState(["type to see results..."])
 
     const [searchbarBlurred, setSearchbarBlurred] = React.useState(false);
 
@@ -24,7 +54,7 @@ export const ConceptSearchBar = () => {
             <FormControl w="60">
                 <FormLabel>Search Concepts</FormLabel>
                 <AutoComplete defaultIsOpen openOnFocus onSelectOption={addConceptToEq} suggestWhenEmpty closeOnBlur={false} onBlur={() => { setSearchbarBlurred(true) }} onFocus={() => { setSearchbarBlurred(false) }} disableFilter={searchbarBlurred} maxSuggestions={100} restoreOnBlurIfEmpty={false}>
-                    <AutoCompleteInput variant="filled" />
+                    <AutoCompleteInput variant="filled" onChange={queryBackend} />
                     <AutoCompleteList color={textColor} bgColor={useColorModeValue("white", "gray.800")} fontWeight={"normal"}>
                         {concepts.map((concept, cid) => (
                             <AutoCompleteItem
