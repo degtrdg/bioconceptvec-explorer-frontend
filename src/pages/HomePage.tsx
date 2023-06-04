addfaa975245dd5d1628177baf37440e13f7cad2
Daniel George <degtrdg@github.com>
2023-06-04 02:01:42 -0500

    Added chat to equation builder
    would like to add embedding context to it later

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import EquationCreator from "../components/EquationCreator";
 import { Grid, GridItem } from "@chakra-ui/react";
 import { ControlPanel } from "../components/ControlPanel";
 import { ConceptSearchBar } from "../components/ConceptSearchBar";
+import ExplorePage from "./ExplorePage";
 
 const HomePage: React.FC = () => {
   return (
@@ -10,28 +11,29 @@ const HomePage: React.FC = () => {
       templateAreas={`"controlpanel controlpanel controlpanel"
                   "searchbar main results"
                   "searchbar footer footer"`}
-      gridTemplateRows={'2fr 8fr 0.5fr'}
-      gridTemplateColumns={'1fr 5fr 2fr'}
-      h='100vh'
+      gridTemplateRows={"2fr 8fr 0.5fr"}
+      gridTemplateColumns={"1fr 5fr 2fr"}
+      h="100vh"
       pt="10vh"
-      gap='1'
-      color='blackAlpha.700'
-      fontWeight='bold'
+      gap="1"
+      color="blackAlpha.700"
+      fontWeight="bold"
     >
-      <GridItem p={3} bg='orange.300' area={'controlpanel'}>
+      <GridItem p={3} bg="orange.300" area={"controlpanel"}>
         <ControlPanel />
       </GridItem>
-      <GridItem p={3} bg='pink.300' area={'searchbar'}>
+      <GridItem p={3} bg="pink.300" area={"searchbar"}>
         <ConceptSearchBar />
       </GridItem>
-      <GridItem p={3} bg='green.300' area={'main'}>
+      <GridItem p={3} bg="green.300" area={"main"}>
         Equation
         <EquationCreator />
       </GridItem>
-      <GridItem p={3} bg='gray.300' area={'results'}>
-        Results
+      <GridItem p={3} bg="gray.300" area={"results"}>
+        {/* Results */}
+        <ExplorePage />
       </GridItem>
-      <GridItem p={3} bg='blue.300' area={'footer'}>
+      <GridItem p={3} bg="blue.300" area={"footer"}>
         Footer
       </GridItem>
     </Grid>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
+  // theme: {
+  //   extend: {
+  //     keyframes: {
+  //       loading: {
+  //         "0%": { backgroundPosition: "-100% 0" },
+  //         "100%": { backgroundPosition: "100% 0" },
+  //       },
+  //     },
+  //     animation: {
+  //       loading: "loading 2s ease-in-out infinite",
+  //     },
+  //   },
+  // },
   plugins: [],
 };
